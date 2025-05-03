ac7e67cc78022a3158a115a5532640ad0a355f08
junsan14 <jusnanjunsan14@gmail.com>
2025-05-03 10:05:09 +0200

    irs2

--- database/migrations/2025_05_03_100042_add_tags_column_to_posts_table.php
+++ database/migrations/2025_05_03_100042_add_tags_column_to_posts_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('posts', function (Blueprint $table) {
-            $table->json('tags')->default(DB::raw('(JSON_ARRAY())'))->nullable()->after('sub_category');
+            $table->json('tags')->nullable()->after('sub_category');
 
         });
     }
