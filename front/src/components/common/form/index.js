8732afbc8735c483a61e6cd89c1b560e9bf5c831
junsan14 <jusnanjunsan14@gmail.com>
2026-07-09 14:58:16 +0200

    edit

--- front/src/components/common/form/index.js
+++ front/src/components/common/form/index.js
@@ -1,5 +1,5 @@
 export { default as Button } from "./Button";
-export { default as Checkbox } from "./Checkbox";
+export { default as Checkbox } from "./CheckBox";
 export { default as FieldError } from "./FieldError";
 export { default as Input } from "./Input";
 export { default as Label } from "./Label";
